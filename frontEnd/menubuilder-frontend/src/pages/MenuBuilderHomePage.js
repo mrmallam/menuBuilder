69ea7b516c0da836027bbbbaaf7d4ab05344c03c
kusumpreetheer <55485344+kusumpreetheer@users.noreply.github.com>
2023-11-26 00:21:12 -0700

    frontendcont

--- frontEnd/menubuilder-frontend/src/pages/MenuBuilderHomePage.js
+++ frontEnd/menubuilder-frontend/src/pages/MenuBuilderHomePage.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import Header from '../components/header';
 import Category from '../components/category';
+import bgshw from '../resources/shwm.jpg'
 
 function MenuBuilderHomePage() {
     return (
       <div className="App">
-        {/* Nav */}
-        <Header />
-        {/* Category */}
-        <Category />
-        {/* Create Category Button */}
-        <button>Create Category</button>
+        <div className=' bg-black h-1/4'>
+            <img src={bgshw} alt='Shawarma Rest Img'/>
+        </div>
+        <div>
+          <h1 className=' flex justify-center align-middle text-3xl font-bold my-2'>
+            Jerusalem Shawarma
+          </h1>
+        </div>
+        <div className=' my-2 mx-auto flex align-middle justify-center w-3/4 h-24 border-2 container shadow-lg'> 
+          <span className=' text-6xl'>
+            +
+          </span>
+        </div>
       </div>
     );
   }
